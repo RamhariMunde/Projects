9801e8e1a65935261b76aae8b3e6f12a5f77c925
Ramhari Munde <89252634+RamhariMunde@users.noreply.github.com>
2024-11-26 15:20:27 +0530

    Add files via upload

---
+++ Keep-note/todo.js
@@ -0,0 +1,19 @@
+
+const keepnote = document.querySelector(".todos");
+const button = document.querySelector(".btn");
+let notes = document.querySelectorAll(".input-box")
+
+button.addEventListener("click", () => {
+    let inputbox = document.createElement("p");
+    let img = document.createElement("img");
+    inputbox.className = "input-box";
+    inputbox.setAttribute("contenteditable","true");
+    img.src = "https://as2.ftcdn.net/v2/jpg/03/46/38/39/1000_F_346383913_JQecl2DhpHy2YakDz1t3h0Tk3Ov8hikq.jpg";
+    keepnote.appendChild(inputbox).appendChild(img);
+ })
+
+   keepnote.addEventListener("click", function(e){
+    if(e.target.tagName === "IMG"){
+        e.target.parentElement.remove();
+    }
+   })
